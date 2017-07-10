bfae9bbfb9b5f4266f1c44f109c42782769ff89d
Jean-Philippe Ruijs <jean-philippe.ruijs@imt.fr>
2017-07-10 13:55:09 +0200

    volume warning

--- src/webaudio.jsx
+++ src/webaudio.jsx
@@ -7,6 +7,8 @@ export default class WebAudio extends React.Component {
         vibrator();
         return (<div>
             <h1>Sweep that frequency!</h1>
+            <h2>WARNING: Set your volume very low before hitting 'PLAY'</h2>
+            
             <Oscillator />
         </div>);
     }
